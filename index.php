1c7b4c600e37bb59f022ab4d9d94a8ac4f8df326
Sang Le <lesan6021@gmail.com>
2019-04-03 17:56:49 -0400

    Add index page

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 1</title>
+</head>
+<body>
+    <h1>Pair Program 1</h1>
+
+
+
+</body>
+</html>
+
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+/**
+ * Created by PhpStorm.
+ * User: darkn
+ * Date: 4/3/2019
+ * Time: 2:27 PM
+ */
+
